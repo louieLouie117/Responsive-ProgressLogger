6468b51f04d9073bb696fcddda870861f4ad3d03
Luis Cardona <cardona-luis@outlook.com>
2021-03-28 07:37:35 -0700

    signIn-complete

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -80,4 +80,8 @@ function hideDeleteSection(e){
 
 const showSignIn= (e)=>{
     document.getElementById("signInContainer").style.marginTop = "0px"
+    document.getElementById("signInContainer").style.transform = "smooth"
+    document.getElementById("signInContainer").style.transition = ".5s"
+
+
 }
