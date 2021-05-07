f50330becc7b757b53a77bb0bb2ee0b52199bb0e
Luis Cardona <cardona-luis@outlook.com>
2021-05-07 13:40:37 -0700

    codeMove-jsCss

--- wwwroot/js/dailyLog.js
+++ wwwroot/js/dailyLog.js
@@ -1,1 +1,77 @@
 // I will need to move all the code that is related to daily log here so that I can keep all the js code separated by partials 
+
+
+
+// show the text and button options
+function showNewLog(e) {
+    document.getElementById("newLogBTN").style.height = "80vh";
+    document.getElementById("newLogBTN").style.position = "relative";
+    document.getElementById("newLogBTN").style.padding = "40px";
+    document.getElementById("saveBTN").style.display = "block";
+    document.getElementById("cancleLog").style.display ="block";
+    
+    
+};
+
+
+function hideNewLog(e){
+    document.getElementById("newLogBTN").style.height = "auto";
+    document.getElementById("saveBTN").style.display = "none";
+    document.getElementById("newLogBTN").style.position = "fixed";
+    document.getElementById("cancleLog").style.display ="none";
+    document.getElementById("newLogBTN").style.padding = "0";
+    document.getElementById("newLogBTN").style.paddingLeft = "20px";
+    document.getElementById("newLogBTN").innerHTML = "";
+
+
+
+
+}
+
+
+
+const ShowDeleteLog = (e)=>{
+    console.log("js File",e.target.id);
+
+    let itemContainer = e.target.id + "Title";
+    let itemToDelete = e.target.id + "Box";
+
+    document.getElementById(itemContainer).style.display = "none";
+    document.getElementById(itemToDelete).style.display = "grid";
+
+
+}
+
+
+
+const HideDeleteLog = (e)=>{
+    console.log(e.target.id);
+
+    let itemContainer = e.target.id + "Title"
+    let itemToDelete = e.target.id + "Box"
+
+    document.getElementById(itemContainer).style.display = "grid";
+    document.getElementById(itemToDelete).style.display = "none";
+    
+
+
+}
+
+
+
+function showDeleteSection(e){
+    let deleteSectionItem = "deleteSection" + e.target.id;
+    console.log(deleteSectionItem);
+    document.getElementById(deleteSectionItem).style.display = "block";
+
+
+}
+
+function hideDeleteSection(e){
+    let deleteSectionItem = "deleteSection" + e.target.id;
+    console.log(deleteSectionItem);
+    
+    document.getElementById(deleteSectionItem).style.display = "none";
+
+}
+
